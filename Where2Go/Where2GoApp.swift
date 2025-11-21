7ca33485ff6715d423bc6c32bb64fc7ab68fb74b
jfelipeschwaab <joaofsp.games@gmail.com>
2025-11-21 14:19:16 -0300

    Initial Commit

---
+++ Where2Go/Where2GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Where2GoApp.swift
+//  Where2Go
+//
+//  Created by João Felipe Schwaab on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Where2GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
